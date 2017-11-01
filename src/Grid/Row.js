1761830d82c3e2f395b4ab011332f228f3fb4dff
AmirTahani <amir.tahani11@gmail.com>
2017-11-01 10:31:29 +0330

    inital commit

---
+++ src/Grid/Row.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
+import PropTypes from 'prop-types';
+
+export const Row = props => {
+    const { containerStyle, size, onPress, activeOpacity, children } = props;
+
+    const styles = StyleSheet.create({
+        container: {
+            flex: size ? size : containerStyle && containerStyle.height ? 0 : 1,
+            flexDirection: 'row',
+        },
+    });
+
+    if (onPress) {
+        return (
+            <TouchableOpacity
+                style={[styles.container, containerStyle && containerStyle]}
+                activeOpacity={activeOpacity}
+                onPress={onPress}
+            >
+                <View {...props}>
+                    {children}
+                </View>
+            </TouchableOpacity>
+        );
+    }
+
+    return (
+        <View
+            style={[styles.container, containerStyle && containerStyle]}
+            {...props}
+        >
+            {children}
+        </View>
+    );
+};
+
+Row.propTypes = {
+    size: PropTypes.number,
+    containerStyle: PropTypes.any,
+    onPress: PropTypes.func,
+    activeOpacity: PropTypes.number,
+    children: PropTypes.any,
+};
+
+Row.defaultProps = {
+    activeOpacity: 1,
+};
+
+export default Row;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import Col from './Grid/Col';
+import Row from './Grid/Row';
+import Grid from './Grid/Grid';
+
+export { Col, Row, Grid };
